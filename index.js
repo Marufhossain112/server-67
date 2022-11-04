8f750bd06e9f7b3f475e101d344e0922264da767
maruf <dev.maruf112@gmail.com>
2022-11-04 16:45:01 +0600

    completed server side

--- index.js
+++ index.js
@@ -44,19 +44,6 @@ async function run() {
       res.send(result);
     });
 
-    // app.get("/orders", async (req, res) => {
-    //   let query = {};
-
-    //   if (req.query.email) {
-    //     query = {
-    //       email: req.query.email,
-    //     };
-    //   }
-    //   const cursor = orderCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-
     // orders api
     app.get("/orders", async (req, res) => {
       let query = {};
@@ -76,9 +63,23 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/orders", async (req, res) => {
-      const order = req.body;
-      const result = await orderCollection.insertOne(order);
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await orderCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
